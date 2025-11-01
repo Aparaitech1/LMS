9f71a41f12e1ad68fae0054e7e20609cc1eea8ab
Sudarshan Patil <sudarshanpatil486@gmail.com>
2025-11-01 12:55:24 +0530

    Cloudinary key correction

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -1,15 +1,16 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 const connectCloudinary = async () => {
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET_KEY,
+    });
 
-  console.log('CLOUDINARY_NAME:', process.env.CLOUDINARY_NAME);
 
-  console.log('✅ Cloudinary connected successfully');
+    console.log('CLOUDINARY_NAME:', process.env.CLOUDINARY_NAME);
+
+    console.log('✅ Cloudinary connected successfully');
 };
 
 export default connectCloudinary;
